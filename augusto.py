02d6eea7c74f245e0bb0bb109c11788f440955be
juuze <jass2@ic.ufal.br>
2021-04-27 16:29:56 -0300

    adiciona grafico de abandono vacinal

--- augusto.py
+++ augusto.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from plotly import express as px
-from datetime import datetime
+from datetime import datetime, timedelta
 from tqdm import tqdm
 
 
@@ -35,12 +35,8 @@ for idx, row in tqdm(df.iterrows()):
 	dict_abandono[row['paciente_id']] += 1'''
 
 
-def gerar_doses_por_dia():
-	df = pd.read_csv('Dados AL.csv', sep=';', low_memory=False)
-	df['vacina_data_processada'] = df['vacina_dataAplicacao'].apply(lambda x : x.split('T')[0])
-	df['vacina_data_processada'] = df['vacina_data_processada'].apply(lambda x : datetime.strptime(x, '%Y-%m-%d').date())
-
-	data = {
+def gerar_doses_por_dia(df):
+	dados = {
 	'data': [],
 	'quantidade': [],
 	'dose': []
@@ -48,16 +44,71 @@ def gerar_doses_por_dia():
 
 	for date in tqdm(df['vacina_data_processada'].unique()):
 		for dose in df['vacina_descricao_dose'].unique():
-			data['data'].append(date)
-			data['dose'].append(dose)
+			dados['data'].append(date)
+			dados['dose'].append(dose)
 			loc = (df['vacina_data_processada'] == date) & (df['vacina_descricao_dose'] == dose)
-			data['quantidade'].append(len(df.loc[loc]))
+			dados['quantidade'].append(len(df.loc[loc]))
 
-	plotdf = pd.DataFrame(data=data)
+	plotdf = pd.DataFrame(data=dados)
 	fig = px.bar(plotdf, x="data", y='quantidade', color="dose", barmode="group")
+
+	return fig
+
+def gerar_abandono_vacinal(df):
+	datas = df['vacina_data_processada'].unique()
+	vacinas = df['vacina_nome'].unique()
+
+	dados = {
+	'data': [],
+	'quantidade': [],
+	'vacina': []
+	}
+
+	janela_2dose = {
+	'Covid-19-AstraZeneca': 84,
+	'Vacina Covid-19 - Covishield': 84,
+	'Covid-19-Coronavac-Sinovac/Butantan': 28
+	}
+
+	dict_abandono = {}
+
+	for idx, row in tqdm(df.iterrows()):
+		data = row['vacina_data_processada']
+		vacina = row['vacina_nome']
+		dose = row['vacina_descricao_dose']
+
+		ndata = data + timedelta(days=janela_2dose[vacina])
+
+		if data not in dict_abandono.keys():
+			dict_abandono[data] = {}
+
+		if ndata not in dict_abandono.keys():
+			dict_abandono[ndata] = {}
+
+		if vacina not in dict_abandono[data].keys():
+			dict_abandono[data][vacina] = 0
+
+		if vacina not in dict_abandono[ndata].keys():
+			dict_abandono[ndata][vacina] = 0
+
+		if dose == '1ªDose':
+			dict_abandono[ndata][vacina] += 1
+		elif dose == '2ªDose':
+			dict_abandono[data][vacina] -= 1
+
+	for data,dict_vacinas in dict_abandono.items():
+		for vacina,quantidade in dict_vacinas.items():
+			if quantidade >= 0:
+				dados['data'].append(data)
+				dados['vacina'].append(vacina)
+				dados['quantidade'].append(quantidade)
+
+
+	plotdf = pd.DataFrame(data=dados)
+	fig = px.bar(plotdf, x="data", y='quantidade', color="vacina", barmode="group")
 
 	return fig
 
 
 if __name__ == '__main__':
-    pass
+	pass
--- plot.py
+++ plot.py
@@ -13,22 +13,62 @@ from datetime import datetime
 import augusto
 
 
+
+df = pd.read_csv('Dados AL.csv', sep=';', low_memory=False)
+df['vacina_data_processada'] = df['vacina_dataAplicacao'].apply(lambda x : x.split('T')[0])
+df['vacina_data_processada'] = df['vacina_data_processada'].apply(lambda x : datetime.strptime(x, '%Y-%m-%d').date())
+df['vacina_descricao_dose'] = df['vacina_descricao_dose'].apply(lambda x : x.replace(u'\xa0', u''))
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Painel Vacinação COVID-19'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Iniciativa do Laboratório de Estatística e Ciência de Dados da UFAL.
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=augusto.gerar_doses_por_dia()
-    )
+    html.Div([
+        html.Div([
+            html.H3('Estado'),
+            dcc.Dropdown(
+		        id='estado-dropdown',
+		        options=[
+		            {'label': 'TODOS', 'value': 'TODOS'},
+		        ],
+		        value='TODOS'
+		    )
+        ], className="one columns"),
+
+        html.Div([
+            html.H3('Município'),
+            dcc.Dropdown(
+		        id='municipio-dropdown',
+		        options=[
+		            {'label': 'TODOS', 'value': 'TODOS'},
+		        ],
+		        value='TODOS'
+		    )
+        ], className="one columns"),
+    ], className="row"),
+
+    html.Div([
+            html.H3('Doses aplicadas por dia'),
+            dcc.Graph(
+		        id='doses-graph',
+		        figure=augusto.gerar_doses_por_dia(df)
+		    )
+    ]),
+
+    html.Div([
+            html.H3('Abandono vacinal por dia'),
+            dcc.Graph(
+		        id='Abandono-graph',
+		        figure=augusto.gerar_abandono_vacinal(df)
+		    )
+    ]),
 ])
 
 if __name__ == '__main__':
