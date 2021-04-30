365993f6d9723c85b555ebbfc5a14f5c9a7ab246
Mauricio Maciel <mrxablau69@gmail.com>
2021-04-30 19:51:21 -0300

    Adiciona grafico de demanda por vacina

--- processamento.py
+++ processamento.py
@@ -31,12 +31,17 @@ def processa_demanda(df):
             os.mkdir(os.path.join('datasets', uf, 'doses_por_dia'))
 
         df_estado = df.loc[df['estabelecimento_uf']==uf].reset_index(drop=True)
-        GeraDados(df_estado).gera_demanda().to_csv('datasets/{}/demanda/TODOS.csv'.format(uf), sep=';', index=False)
-        GeraDados(df_estado).gerar_doses_por_dia().to_csv('datasets/{}/doses_por_dia/TODOS.csv'.format(uf), sep=';', index=False)
+        data_estado = GeraDados(df_estado)
+        data_estado.gera_demanda().to_csv('datasets/{}/demanda/TODOS.csv'.format(uf), sep=';', index=False)
+        data_estado.gera_demanda(tipo_vacina='coronavac').to_csv('datasets/{}/demanda/TODOS_coronavac.csv'.format(uf), sep=';', index=False)
+        data_estado.gera_demanda(tipo_vacina='astrazeneca').to_csv('datasets/{}/demanda/TODOS_astrazeneca.csv'.format(uf), sep=';', index=False)
+        data_estado.gerar_doses_por_dia().to_csv('datasets/{}/doses_por_dia/TODOS.csv'.format(uf), sep=';', index=False)
         for cidade in df["estabelecimento_municipio_nome"].unique():
             df_municipio = df.loc[df['estabelecimento_municipio_nome']==cidade].reset_index(drop=True)
             data = GeraDados(df_municipio)
             data.gera_demanda().to_csv('datasets/{}/demanda/{}.csv'.format(uf, cidade), sep=';', index=False)
+            data.gera_demanda(tipo_vacina='astrazeneca').to_csv('datasets/{}/demanda/{}_astrazeneca.csv'.format(uf, cidade), sep=';', index=False)
+            data.gera_demanda(tipo_vacina='coronavac').to_csv('datasets/{}/demanda/{}_coronavac.csv'.format(uf, cidade), sep=';', index=False)
             data.gerar_doses_por_dia().to_csv('datasets/{}/doses_por_dia/{}.csv'.format(uf, cidade), sep=';', index=False)
 
 if __name__ == '__main__':
