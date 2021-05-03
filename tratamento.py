830721ff99e26ab8cf12502d5c3aa86d39dc1580
Mauricio Maciel <mrxablau69@gmail.com>
2021-05-03 08:32:00 -0300

    Usa funçao interna do pandas ao inves de um for na instancia process_source_data

--- tratamento.py
+++ tratamento.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy
 
 DATE_COLUMN = {
-    1: "vacina_dose_1",
-    2: "vacina_dose_2"
+    '1ªDose': "vacina_dose_1",
+    '2ªDose': "vacina_dose_2",
 }
 
 DOSE_OFFSET = {
     "Covid-19-AstraZeneca": 84,
-    "Covid-19-Coronavac-Sinovac/Butantan": 28
+    "Covid-19-Coronavac-Sinovac/Butantan": 28,
+    "Vacina covid-19 - BNT162b2 - BioNTech/Fosun Pharma/Pfizer": 21
 }
 
 def compute_delay(row):
@@ -18,12 +19,12 @@ def compute_delay(row):
     return row["vacina_dose_2"] - (row["vacina_dose_1"] + timedelta(days=row["vacina_janela"]))
 
 def abandon_rate(data, date, vaccine_name):
-    data_ = data[data.vacina_nome == vaccine_name].reset_index(drop=True)
-    data_ = data_[data_.vacina_dose_1 < date - timedelta(days=DOSE_OFFSET[vaccine_name])]
-    first_dose_count = data_.shape[0]
+    data = data.loc[data['vacina_nome']==vaccine_name].reset_index(drop=True)
+    data = data.loc[data['vacina_dose_1'] < date - timedelta(days=DOSE_OFFSET[vaccine_name])]
+    first_dose_count = data.shape[0]
     if not first_dose_count:
         return None
-    second_dose_count = data_.dropna(subset=["vacina_dose_2"])[data_.vacina_dose_2 <= date].shape[0]
+    second_dose_count = data.dropna(subset=["vacina_dose_2"]).loc[data.dropna(subset=["vacina_dose_2"])['vacina_dose_2'] <= date].shape[0]
     
     return 100 * (first_dose_count - second_dose_count) / first_dose_count
 
@@ -31,7 +32,7 @@ def abandon_series(vaccine_name, date_interval, data):
     begin_date = date_interval[0] + timedelta(days=DOSE_OFFSET[vaccine_name])
     abandon_date_interval = pd.date_range(
         begin_date, datetime.today().date(), freq='d'
-    ).date
+    ).date()
     ab_df = pd.DataFrame(
         index=abandon_date_interval,
         data=[
@@ -75,7 +76,7 @@ def delay_series(data, date_interval):
             nul_delay.at[d["vacina_dose_2"], "nul-" + d["vacina_nome"]] += 1
             nul_delay_count += 1
 
-    return pd.concat([neg_delay, nul_delay, pos_delay]), (neg_delay_count, nul_delay_count, pos_delay_count)
+    return pd.concat([neg_delay, nul_delay, pos_delay])
 
 
 def prazo(row):
@@ -92,92 +93,61 @@ def prazo(row):
 
 class Tratamento():
     def __init__(self, df):
-        self.df = df.drop_duplicates(subset=['paciente_id', 'vacina_dataAplicacao', 'vacina_descricao_dose'], keep='first').reset_index(drop=True)
-
+        df = df[['vacina_dataAplicacao', 'vacina_descricao_dose', 'paciente_id', 'vacina_nome']]
+        df = df.drop_duplicates(subset=['paciente_id', 'vacina_dataAplicacao', 'vacina_descricao_dose'], keep='first').reset_index(drop=True)
+        df['vacina_descricao_dose'] = df['vacina_descricao_dose'].apply(lambda x : x.replace(u'\xa0', u''))
+        df['vacina_dataAplicacao'] = df['vacina_dataAplicacao'].apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d').date())
+        df = df.loc[(df['vacina_descricao_dose']=='1ªDose') | (df['vacina_descricao_dose']=='2ªDose')].reset_index(drop=True)
+        self.df = df
+        
     def gera_df_primeira_dose_apenas(self):
         """
         Objeto para gerar dataframe das pessoas que tomaram apenas a primeira dose da vacina
         """
         df = self.df
         df = df.loc[df.duplicated(subset=['paciente_id'], keep=False)==False].reset_index(drop=True)
-        df['vacina_dataAplicacao'] = df['vacina_dataAplicacao'].apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d').date())
         df = df.sort_values(by=['vacina_dataAplicacao'], ascending=True).reset_index(drop=True)
         return df
 
-    def gera_df_duas_doses(self):
-        """
-        Objeto para gerar dataframe das pessoas que tomaram as duas doses
-        """
-        df = self.df
-
-        df = df.loc[df.duplicated(subset=['paciente_id'], keep=False)==True].reset_index(drop=True)
-        df['vacina_dataAplicacao'] = df['vacina_dataAplicacao'].apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d'))
-
-        drop_anomalias = df.groupby(['paciente_id']).size().to_frame(name='count').reset_index(drop=False)
-        drop_anomalias = drop_anomalias.loc[drop_anomalias['count']==2]
-
-        df = df.loc[df['paciente_id'].isin(drop_anomalias['paciente_id'])]
-        df = df.sort_values(by=['paciente_id', 'vacina_dataAplicacao'], ascending=True)
-
-        condicao1 = (df['paciente_id'].isin(df.iloc[::2].loc[df.iloc[::2]['vacina_descricao_dose']=='\xa0\xa0\xa0\xa01ª\xa0Dose']['paciente_id'])) & (df['paciente_id'].isin(df.iloc[1::2].loc[df.iloc[1::2]['vacina_descricao_dose']=='\xa0\xa0\xa0\xa02ª\xa0Dose']['paciente_id']))
-        condicao2 = (df['paciente_id'].isin(df.iloc[::2].loc[df.iloc[::2]['vacina_descricao_dose']=='\xa0\xa0\xa0\xa02ª\xa0Dose']['paciente_id'])) & (df['paciente_id'].isin(df.iloc[1::2].loc[df.iloc[1::2]['vacina_descricao_dose']=='\xa0\xa0\xa0\xa01ª\xa0Dose']['paciente_id']))
-
-        df = df.loc[(condicao1) | (condicao2)].sort_values(by=['paciente_id', 'vacina_dataAplicacao'], ascending=True).reset_index(drop=True)
-
-        serie_segunda_dose = df.iloc[1::2]['vacina_dataAplicacao'].reset_index(drop=True)
-        df = df.iloc[::2].reset_index(drop=True)
-        df['dataSegundaDose'] = serie_segunda_dose
-        df['diferenca'] = df['dataSegundaDose'] - df['vacina_dataAplicacao']
-        df['diferenca'] = df['diferenca'].apply(lambda x: x.days)
-
-        df['atrasados'] = df.apply(lambda row: prazo(row), axis=1)
-
-        return df
-
     def gerar_doses_por_dia(self):
-        df = self.df
-        df['vacina_descricao_dose'] = df['vacina_descricao_dose'].apply(lambda x : x.replace(u'\xa0', u''))
-        df['vacina_dataAplicacao'] = df['vacina_dataAplicacao'].apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d').date())
+        df = self.df[['vacina_dataAplicacao', 'vacina_descricao_dose']]
         df = df.groupby(by=['vacina_descricao_dose', 'vacina_dataAplicacao']).size().to_frame(name='Quantidade').reset_index(drop=False).rename(columns={'vacina_dataAplicacao': 'Data', 'vacina_descricao_dose': 'Dose Aplicada'})
         return df
 
     def process_source_data(self):
         dataframe = self.df
-        dataframe.loc[:, 'vacina_aplicacao'] = dataframe.loc[:, 'vacina_dataAplicacao'].apply(lambda x : x.split('T')[0]).apply(lambda x: datetime.strptime(x, '%Y-%m-%d').date())
-        dataframe.loc[:, 'vacina_dose'] = dataframe.loc[:, 'vacina_descricao_dose'].apply(lambda x : x.replace(u'\xa0', u''))
-        dataframe = dataframe[dataframe.columns.drop(["vacina_dataAplicacao", "vacina_descricao_dose"])]
+        dataframe = dataframe.rename(columns={'vacina_dataAplicacao': 'vacina_aplicacao', 'vacina_descricao_dose': 'vacina_dose'})
         dataframe.vacina_nome = dataframe.vacina_nome.apply(lambda x: "Covid-19-AstraZeneca" if x == "Vacina Covid-19 - Covishield" else x)
 
-        data = {p_id: {"paciente_id": p_id} for p_id in dataframe.paciente_id}
-
-        for rec in dataframe.to_dict(orient="records"):
-            data[rec["paciente_id"]]["vacina_janela"] = DOSE_OFFSET[rec["vacina_nome"]]
-            data[rec["paciente_id"]]["vacina_nome"] = rec["vacina_nome"]
-            data[rec["paciente_id"]][DATE_COLUMN[rec["vacina_dose"]]] = rec["vacina_aplicacao"]
-
-        data = pandas.DataFrame(data=data.values())
+        dataframe = dataframe.drop_duplicates(subset=['paciente_id', 'vacina_dose']).reset_index(drop=True).pivot(index=['paciente_id', 'vacina_nome'], columns='vacina_dose', values='vacina_aplicacao').reset_index(drop=False)
+        dataframe['vacina_janela'] = dataframe['vacina_nome'].apply(lambda x: DOSE_OFFSET[x])
+        dataframe = dataframe.rename(columns={'1ªDose': 'vacina_dose_1', '2ªDose': 'vacina_dose_2'})
 
         # ## Importante - tratamento de anomalias
         # 
         # Foram encontrados registros duvidosos onde:
         # 
-        # * Não existe data de vacinação para a 1a dose. (3218 registros)
-        # * A data de vacinação da segunda dose é anterior ou igual a da primeira. (688 registros)
-        anomaly_count = {
-            "Sem data da primeira dose":  data[data.vacina_dose_1.isna()].shape[0],
-            "Data da primeira dose é maior que data da segunda dose": data[data.vacina_dose_2 <= data.vacina_dose_1].shape[0]
-        }
+        # * Nome da vacina da primeira dose é diferente da primeira
+        # * Não existe data de vacinação para a 1a dose.
+        # * A data de vacinação da segunda dose é anterior ou igual a da primeira.
+        #anomaly_count = {
+        #    "Sem data da primeira dose":  data[data.vacina_dose_1.isna()].shape[0],
+        #    "Data da primeira dose é maior que data da segunda dose": data[data.vacina_dose_2 <= data.vacina_dose_1].shape[0]
+        #}
+
+        # Removendo registros com divergência no nome da vacina
+        dataframe = dataframe.loc[dataframe.duplicated(subset=['paciente_id'], keep=False)==False].reset_index(drop=True)
 
         # Removendo os registros (sem data de 1a dose):
-        data = data.dropna(subset=["vacina_dose_1"]).reset_index(drop=True)
+        dataframe = dataframe.dropna(subset=["vacina_dose_1"]).reset_index(drop=True)
 
         # Removendo registros (data 2a dose anterior ou igual à 1a):
-        data = data[data.vacina_dose_1 < data.vacina_dose_2].reset_index(drop=True)
+        dataframe = dataframe[dataframe.vacina_dose_1 < dataframe.vacina_dose_2].reset_index(drop=True)
 
         # Calculando o atraso (em dias) entre a data prevista de aplicação da 2a dose e a data observada:
-        data['vacina_atraso'] = data.apply(compute_delay, axis=1).dt.days
+        dataframe['vacina_atraso'] = dataframe.apply(compute_delay, axis=1).dt.days
 
-        return data
+        return dataframe
 
 class GeraDados():
     def __init__(self, df):
@@ -231,9 +201,9 @@ class GeraDados():
 
     def gera_serie_atraso(self):
         data = self.process_source_data
-        
+        date_interval = pd.date_range(data.vacina_dose_1.min(), datetime.today().date(), freq='d').date
         # Calculando quantidade de pessoas atrasadas (atraso <, > e = 0) por dia:
-        delay_df, (neg, nul, pos) = delay_series(date_interval=date_interval, data=data)
+        delay_df = delay_series(date_interval=date_interval, data=data)
 
         return delay_df
 
@@ -243,13 +213,13 @@ class GeraDados():
         date = datetime.today().date()
         rate_covac = abandon_rate(data=data, date=date, vaccine_name="Covid-19-Coronavac-Sinovac/Butantan")
         rate_astra = abandon_rate(data=data, date=date, vaccine_name="Covid-19-AstraZeneca")
-        
+        date_interval = pd.date_range(data.vacina_dose_1.min(), datetime.today().date(), freq='d').date
         # Calculando série histórica do abandono vacinal:
         ab_df_covac = abandon_series(vaccine_name="Covid-19-Coronavac-Sinovac/Butantan", date_interval=date_interval, data=data)
         ab_df_astra = abandon_series(vaccine_name="Covid-19-AstraZeneca", date_interval=date_interval, data=data)
 
         # Série historica abandono
-        ab_df = pandas.concat([ab_df_covac, ab_df_astra])
+        ab_df = pd.concat([ab_df_covac, ab_df_astra])
 
         return ab_df
 
