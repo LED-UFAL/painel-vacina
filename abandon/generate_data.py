ea5076459226f7d7aaa40da6d5c1e46ba1c722da
hugotallys <htmo@ic.ufal.br>
2021-05-03 11:55:21 -0300

    caminho csv como parametro adiciona documentaçao

--- abandon/generate_data.py
+++ abandon/generate_data.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from tqdm import tqdm
 
@@ -7,31 +8,41 @@ from process_data import process_source_data
 
 
 if __name__ == "__main__":
+    
+    """
+    Script de geração dos dados de visualização para atraso e taxa de abandono vacinal.
+    Para executá-lo no terminal:
+    $ python abandon/generate_data.py csv_path='caminho/para/o/arquivo.csv'
+    """
 
-    CSV_PATH = "datasets/source_data/AL/dados-vacina.csv"
+    # Lendo os parâmetros de entrada e tratando
 
-    dataframe = process_source_data(path=CSV_PATH)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csv_path")
+    args = parser.parse_args()
+    csv_path = args.csv_path.split("=")[1]
 
+    # Lendo o dataframe e construindo a lista de unidades federativas (Loop I)
+
+    dataframe = process_source_data(path=csv_path)
     unique_uf = dataframe.estabelecimento_uf.unique()
-
     pbar_uf = tqdm(unique_uf, desc="Loop UF")
 
     for uf in pbar_uf:    
-        
-        dataframe_ = dataframe[dataframe.estabelecimento_uf == uf]
+
+        # Diretório onde serão armazenados os dados da visualização
 
         if not os.path.exists(os.path.join('datasets', uf)):
             os.mkdir(os.path.join('datasets', uf))
-
         if not os.path.exists(os.path.join('datasets', uf, 'abandono-atraso-vacinal')):
             os.mkdir(os.path.join('datasets', uf, 'abandono-atraso-vacinal'))
-
         dest_vis_dir = f"datasets/{uf}/abandono-atraso-vacinal"
 
-        pbar = tqdm(dataframe_.estabelecimento_municipio_nome.unique())
+        # Filtrando o dataframe para cada UF e processando os dados por município (Loop II)
+        
+        dataframe_ = dataframe[dataframe.estabelecimento_uf == uf]
+        pbar = tqdm(dataframe_.estabelecimento_municipio_nome.unique().tolist() + ["TODOS"])
 
         for municipio in pbar:
-            
             pbar.set_description(f"Salvando dados para {uf} ... {municipio} ")
-
             process_data_main(uf=uf, municipio=municipio, dataframe=dataframe_, dest_vis_dir=dest_vis_dir, overwrite=True)
