2b73282dd53de78e4a084cb0d2ee9248eb70a939
hugotallys <htmo@ic.ufal.br>
2021-05-04 21:12:50 -0300

    corrige assinatura da função e retira função da taxa de abandono

--- augusto.py
+++ augusto.py
@@ -46,26 +46,13 @@ def plotar_demanda_por_vacina(uf='TODOS', municipio='TODOS', grafico='DEMANDA PO
     )
 	return fig
 
-def plot_delay(uf, municipio, signal):
+
+def plot_delay(uf, municipio, prefix):
     path = CURRENT_DIR + os.path.join(f"/datasets/{uf}/abandono-atraso-vacinal", municipio)
     delay_df = pd.read_csv(os.path.join(path, "serie-atraso.csv"), sep=";", index_col=0)
     delay_df.index = delay_df.index.to_series().apply(lambda x: datetime.strptime(x, '%Y-%m-%d').date())
-    fig = px.bar(delay_df[[c for c in delay_df.columns if signal + "-" in c]], barmode="group")
+    fig = px.bar(delay_df[[c for c in delay_df.columns if prefix + " - " in c]], barmode="group")
     fig.update_layout(
         xaxis=dict(title="Data"), yaxis=dict(title="Quantidade"), legend=dict(title="Tipo Vacina")
     )
     return fig
-
-def plot_abandon(uf, municipio):
-    path = CURRENT_DIR + '/'+os.path.join(f"/datasets/{uf}/abandono-atraso-vacinal", municipio)
-    ab_df = pd.read_csv(os.path.join(path, "serie-abandono.csv"), sep=";", index_col=0)
-    ab_df.index = ab_df.index.to_series().apply(lambda x: datetime.strptime(x, '%Y-%m-%d').date())
-    fig = px.line(ab_df)
-    fig.update_layout(
-        xaxis=dict(title="Data"), yaxis=dict(title="Taxa de abandono"), legend=dict(title="Tipo Vacina")
-    )
-    return fig
-
-
-if __name__ == '__main__':
-	pass
